80e6cbeb2458f67806df40ac99534553ddb16ceb
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-08-04 23:59:09 +0100

    improve generic behaviour methods for fuel handling

--- behaviours/generic_behaviour.py
+++ behaviours/generic_behaviour.py
@@ -2,8 +2,10 @@ import json
 from straders_sdk import SpaceTraders
 from time import sleep
 from straders_sdk.ship import Ship
+from straders_sdk.models import Waypoint
 from straders_sdk.utils import set_logging
 import logging
+import math
 
 
 class Behaviour:
@@ -49,16 +51,23 @@ class Behaviour:
         pass
 
     def ship_intrasolar(self, target_wp_symbol: "str"):
-        if self.ship.nav.waypoint_symbol != target_wp_symbol:
-            if self.ship.nav.status == "DOCKED":
-                self.st.ship_orbit(self.ship)
-            resp = self.st.ship_move(self.ship, target_wp_symbol)
+        st = self.st
+        ship = self.ship
+        wp = self.st.waypoints_view_one(ship.nav.system_symbol, target_wp_symbol)
+
+        fuel_cost = self.determine_fuel_cost(self.ship, wp)
+        if fuel_cost > ship.fuel_current:
+            self.refuel_if_low()
+        if ship.nav.waypoint_symbol != target_wp_symbol:
+            if ship.nav.status == "DOCKED":
+                st.ship_orbit(self.ship)
+            resp = st.ship_move(self.ship, target_wp_symbol)
             if not resp:
                 return False
 
             sleep_until_ready(self.ship)
-            self.ship.nav.status = "IN_ORBIT"
-            self.ship.nav.waypoint_symbol = target_wp_symbol
+            ship.nav.status = "IN_ORBIT"
+            ship.nav.waypoint_symbol = target_wp_symbol
             return resp
 
     def extract_till_full(self, cargo_to_target: list = None):
@@ -86,8 +95,26 @@ class Behaviour:
 
     def refuel_if_low(self):
         ship = self.ship
-        if ship.fuel_current < ship.fuel_capacity * 0.5:
+        refuel_points = self.st.find_waypoints_by_trait(
+            self.ship.nav.system_symbol, "MARKETPLACE"
+        )
+        nearest_refuel_wp = None
+        nearest_refuel_distance = 99999
+        for refuel_point in refuel_points:
+            distance = self.distance_from_ship(ship, refuel_point)
+            if distance < nearest_refuel_distance:
+                nearest_refuel_distance = distance
+                nearest_refuel_wp = refuel_point
+        if nearest_refuel_wp is not None:
+            previous_state = None
+            if self.determine_fuel_cost(ship, nearest_refuel_wp) > ship.fuel_current:
+                previous_state = ship.nav.flight_mode
+                self.st.ship_patch_nav(ship, "DRIFT")
+            self.ship_intrasolar(nearest_refuel_wp.symbol)
+            self.st.ship_dock(ship)
             self.st.ship_refuel(ship)
+            if previous_state:
+                self.st.ship_patch_nav(ship, previous_state)
 
     def sell_all_cargo(self, exceptions: list = []):
         ship = self.ship
@@ -102,6 +129,41 @@ class Behaviour:
     def sleep_until_ready(self):
         sleep_until_ready(self.ship)
 
+    def determine_fuel_cost(self, ship: "Ship", target_wp: "Waypoint") -> int:
+        st = self.st
+        source = st.waypoints_view_one(ship.nav.system_symbol, ship.nav.waypoint_symbol)
+
+        speed = {"CRUISE": 1, "DRIFT": 0, "BURN": 2, "STEALTH": 1}
+        return int(
+            max(
+                distance_between_wps(source, target_wp) * speed[ship.nav.flight_mode], 1
+            )
+        )
+
+    def determine_travel_time(self, ship: "Ship", target_wp: "Waypoint") -> int:
+        st = self.st
+        source = st.waypoints_view_one(ship.nav.system_symbol, ship.nav.waypoint_symbol)
+
+        distance = math.sqrt(
+            (target_wp.x - source.x) ** 2 + (target_wp.y - source.y) ** 2
+        )
+        multiplier = {"CRUISE": 15, "DRIFT": 150, "BURN": 7.5, "STEALTH": 30}
+        (
+            math.floor(round(max(1, distance)))
+            * (multiplier[ship.nav.flight_mode] / ship.engine.speed)
+            + 15
+        )
+
+    def distance_from_ship(self, ship: Ship, target_wp: Waypoint) -> float:
+        source = self.st.waypoints_view_one(
+            ship.nav.system_symbol, ship.nav.waypoint_symbol
+        )
+        return distance_between_wps(source, target_wp)
+
 
 def sleep_until_ready(ship: "Ship"):
     sleep(max(ship.seconds_until_cooldown, ship.nav.travel_time_remaining))
+
+
+def distance_between_wps(source: Waypoint, target_wp: Waypoint) -> float:
+    return math.sqrt((target_wp.x - source.x) ** 2 + (target_wp.y - source.y) ** 2)
