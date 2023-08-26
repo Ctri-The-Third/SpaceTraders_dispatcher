ff3e8620b85aa44145b03a00dba1c8c690eeff53
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-08-27 00:05:20 +0100

    Update conductorWK8.py

--- conductorWK8.py
+++ conductorWK8.py
@@ -300,7 +300,7 @@ def stage_3(client: SpaceTraders):
     #
     # Scale up to 30 miners and 3 haulers. Prioritise a hauler if we've got too many drones.
     #
-    if len(haulers) <= math.floor(len(excavators) / extractors_per_hauler):
+    if len(haulers) < math.floor(len(excavators) / extractors_per_hauler):
         ship = maybe_buy_ship_hq_sys(client, "SHIP_LIGHT_HAULER")
         if ship:
             set_behaviour(ship.name, BHVR_RECEIVE_AND_FULFILL, hauler_params)
