d8cc0c5f9dc0116469bebe42b18c712c2b593e7d
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-11-16 20:53:01 +0000

    add locking to generic behaviour template

--- behaviours/generic_behaviour.py
+++ behaviours/generic_behaviour.py
@@ -752,10 +752,12 @@ def sleep_until_ready(ship: "Ship"):
 if __name__ == "__main__":
     import sys
 
+    sys.path.append(".")
+    from dispatcherWK16 import lock_ship
+
     set_logging(level=logging.DEBUG)
     agent = sys.argv[1] if len(sys.argv) > 2 else "CTRI-U-"
     ship_number = sys.argv[2] if len(sys.argv) > 2 else "1"
     ship = f"{agent}-{ship_number}"
     bhvr = Behaviour(agent, ship, {})
-    bhvr.ship_intrasolar("X1-U49-A2")
     bhvr.sell_all_cargo()
