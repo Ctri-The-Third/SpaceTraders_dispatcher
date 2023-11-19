77e1a7fb4ecdf25f6f10dfaf183c384f48b54f2b
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-11-19 17:18:40 +0000

    conductor has a minimum safety margin of 30k built in

--- conductorWK18.py
+++ conductorWK18.py
@@ -180,7 +180,6 @@ class FuelManagementConductor:
     def hourly_update(self):
         # if we have a fuel shipper, give it all the fuel shipping tasks possible
         # if not, give the exporter a refuel task for 1 (maybe 2) points.
-        self.safety_margin = 0
 
         possible_ships = self.haulers + self.commanders
 
@@ -207,7 +206,7 @@ class FuelManagementConductor:
     def quarterly_update(self):
         # if we have a fuel shipper, give it all the fuel shipping tasks possible
         # if not, give the exporter a refuel task for 1 (maybe 2) points.
-        self.safety_margin = 0
+        self.safety_margin = 30000
 
         possible_ships = self.haulers + self.commanders
         # self.set_refinery_behaviours(possible_ships)
