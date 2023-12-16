7f82d72a40783bf6aafa50240b9123898c9b3557
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-12-16 19:32:38 +0000

    moved all the dispatcher behaviours into their own file for ease of reading

---
+++ behaviour_constants.py
@@ -0,0 +1,81 @@
+from behaviours.extract_and_chill import (
+    ExtractAndChill,
+    BEHAVIOUR_NAME as BHVR_EXTRACT_AND_CHILL,
+)
+from behaviours.buy_and_sell_dripfeed import (
+    BuyAndSellDripfeed,
+    BEHAVIOUR_NAME as BHVR_BUY_AND_SELL_DRIPFEED,
+)
+from behaviours.extract_and_sell import (
+    ExtractAndGoSell,
+    BEHAVIOUR_NAME as BHVR_EXTRACT_AND_GO_SELL,
+)
+from behaviours.receive_and_fulfill import (
+    ReceiveAndFulfillOrSell_3,
+    BEHAVIOUR_NAME as BHVR_RECEIVE_AND_FULFILL,
+)
+from behaviours.generic_behaviour import Behaviour
+from behaviours.extract_and_transfer import (
+    ExtractAndTransfer_8,
+    BEHAVIOUR_NAME as BHVR_EXTRACT_AND_TRANSFER,
+)
+from behaviours.explore_system import (
+    ExploreSystem,
+    BEHAVIOUR_NAME as BHVR_EXPLORE_SYSTEM,
+)
+from behaviours.monitor_cheapest_price import (
+    MonitorCheapestShipyard,
+    BEHAVIOUR_NAME as BHVR_MONITOR_CHEAPEST_PRICE,
+)
+from behaviours.buy_and_deliver_or_sell import (
+    BuyAndDeliverOrSell_6,
+    BEHAVIOUR_NAME as BHVR_BUY_AND_DELIVER_OR_SELL,
+)
+
+from behaviours.receive_and_refine import (
+    ReceiveAndRefine,
+    BEHAVIOUR_NAME as BHVR_RECEIVE_AND_REFINE,
+)
+from behaviours.extract_and_fulfill import (
+    ExtractAndFulfill_7,
+    BEHAVIOUR_NAME as BHVR_EXTRACT_AND_FULFILL,
+)
+
+from behaviours.upgrade_ship_to_specs import (
+    FindMountsAndEquip,
+    BEHAVIOUR_NAME as BHVR_UPGRADE_TO_SPEC,
+)
+from behaviours.chill_and_survey import (
+    ChillAndSurvey,
+    BEHAVIOUR_NAME as BHVR_CHILL_AND_SURVEY,
+)
+from behaviours.refuel_all_fuel_exchanges_in_system import (
+    RefuelAnExchange,
+    BEHAVIOUR_NAME as BHVR_REFUEL_ALL_IN_SYSTEM,
+)
+from behaviours.single_stable_trade import (
+    SingleStableTrade,
+    BEHAVIOUR_NAME as BHVR_SINGLE_STABLE_TRADE,
+)
+from behaviours.monitor_specific_location import (
+    MonitorPrices,
+    BEHAVIOUR_NAME as BHVR_MONITOR_SPECIFIC_LOCATION,
+)
+from behaviours.take_from_extractors_and_fulfill import (
+    TakeFromExactorsAndFulfillOrSell_9,
+    BEHAVIOUR_NAME as BHVR_TAKE_FROM_EXTRACTORS_AND_FULFILL,
+)
+from behaviours.siphon_and_chill import (
+    SiphonAndChill,
+    BEHAVIOUR_NAME as BHVR_SIPHON_AND_CHILL,
+)
+from behaviours.manage_specific_export import (
+    ManageSpecifcExport,
+    BEHAVIOUR_NAME as BHVR_MANAGE_SPECIFIC_EXPORT,
+)
+from behaviours.construct_a_jumpgate import (
+    ConstructJumpgate,
+    BEHAVIOUR_NAME as BHVR_CONSTRUCT_JUMP_GATE,
+)
+
+from behaviours.chain_trade import ChainTrade, BEHAVIOUR_NAME as BHVR_CHAIN_TRADE
--- dispatcherWK16.py
+++ dispatcherWK16.py
@@ -15,85 +15,7 @@ from straders_sdk import SpaceTraders
 from straders_sdk.request_consumer import RequestConsumer
 from straders_sdk.models import Waypoint
 from straders_sdk.utils import set_logging, waypoint_slicer, get_and_validate
-from behaviours.extract_and_chill import (
-    ExtractAndChill,
-    BEHAVIOUR_NAME as BHVR_EXTRACT_AND_CHILL,
-)
-from behaviours.buy_and_sell_dripfeed import (
-    BuyAndSellDripfeed,
-    BEHAVIOUR_NAME as BHVR_BUY_AND_SELL_DRIPFEED,
-)
-from behaviours.extract_and_sell import (
-    ExtractAndGoSell,
-    BEHAVIOUR_NAME as BHVR_EXTRACT_AND_GO_SELL,
-)
-from behaviours.receive_and_fulfill import (
-    ReceiveAndFulfillOrSell_3,
-    BEHAVIOUR_NAME as BHVR_RECEIVE_AND_FULFILL,
-)
-from behaviours.generic_behaviour import Behaviour
-from behaviours.extract_and_transfer import (
-    ExtractAndTransfer_8,
-    BEHAVIOUR_NAME as BHVR_EXTRACT_AND_TRANSFER,
-)
-from behaviours.explore_system import (
-    ExploreSystem,
-    BEHAVIOUR_NAME as BHVR_EXPLORE_SYSTEM,
-)
-from behaviours.monitor_cheapest_price import (
-    MonitorCheapestShipyard,
-    BEHAVIOUR_NAME as BHVR_MONITOR_CHEAPEST_PRICE,
-)
-from behaviours.buy_and_deliver_or_sell import (
-    BuyAndDeliverOrSell_6,
-    BEHAVIOUR_NAME as BHVR_BUY_AND_DELIVER_OR_SELL,
-)
 
-from behaviours.receive_and_refine import (
-    ReceiveAndRefine,
-    BEHAVIOUR_NAME as BHVR_RECEIVE_AND_REFINE,
-)
-from behaviours.extract_and_fulfill import (
-    ExtractAndFulfill_7,
-    BEHAVIOUR_NAME as BHVR_EXTRACT_AND_FULFILL,
-)
-
-from behaviours.upgrade_ship_to_specs import (
-    FindMountsAndEquip,
-    BEHAVIOUR_NAME as BHVR_UPGRADE_TO_SPEC,
-)
-from behaviours.chill_and_survey import (
-    ChillAndSurvey,
-    BEHAVIOUR_NAME as BHVR_CHILL_AND_SURVEY,
-)
-from behaviours.refuel_all_fuel_exchanges_in_system import (
-    RefuelAnExchange,
-    BEHAVIOUR_NAME as BHVR_REFUEL_ALL_IN_SYSTEM,
-)
-from behaviours.single_stable_trade import (
-    SingleStableTrade,
-    BEHAVIOUR_NAME as BHVR_SINGLE_STABLE_TRADE,
-)
-from behaviours.monitor_specific_location import (
-    MonitorPrices,
-    BEHAVIOUR_NAME as BHVR_MONITOR_SPECIFIC_LOCATION,
-)
-from behaviours.take_from_extractors_and_fulfill import (
-    TakeFromExactorsAndFulfillOrSell_9,
-    BEHAVIOUR_NAME as BHVR_TAKE_FROM_EXTRACTORS_AND_FULFILL,
-)
-from behaviours.siphon_and_chill import (
-    SiphonAndChill,
-    BEHAVIOUR_NAME as BHVR_SIPHON_AND_CHILL,
-)
-from behaviours.manage_specific_export import (
-    ManageSpecifcExport,
-    BEHAVIOUR_NAME as BHVR_MANAGE_SPECIFIC_EXPORT,
-)
-from behaviours.construct_a_jumpgate import (
-    ConstructJumpgate,
-    BEHAVIOUR_NAME as BHVR_CONSTRUCT_JUMP_GATE,
-)
 from behaviours.sell_or_jettison_all_cargo import ( SellOrDitch, BEHAVIOUR_NAME as BHVR_SELL_OR_JETTISON_ALL_CARGO)
 from behaviours.scan_behaviour import ScanInBackground
 from behaviours.generic_behaviour import Behaviour
@@ -112,6 +34,8 @@ RQ_HEAVY_FREIGHTER = "HEAVY_FREIGHTER"
 RQ_ANY_FREIGHTER = "ANY_FREIGHTER"
 RQ_CARGO = "_CARGO"
 RQ_FUEL = "_FUEL"
+
+from behaviour_constants import * 
 
 behaviours_and_classes = {
     BHVR_EXTRACT_AND_GO_SELL: ExtractAndGoSell,
